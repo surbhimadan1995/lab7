969bbf0dddea9a259bb808bcbfa78a89d8c5483b
Beverly <beverly_tai@brown.edu>
2015-04-05 14:36:16 -0400

    Insertion Sort implemented

--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -6,24 +6,28 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		//Generates a random array
 		int[] unsortedArray = new int[(int) (Math.random()*20.0)];
 		for (int i = 0; i < unsortedArray.length; i++) {
 			unsortedArray[i] = (int) (Math.random()*20.0);
 		}
-		
+
 		System.out.println("Unsorted Array:");
-		
+
 		for (int i: unsortedArray) {
 				System.out.print(i + " ");
 		}
 		System.out.println();
-		
+
 		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		
-		
+		int[] sortedArray = InsertionSort.insertionSort(unsortedArray);
+		System.out.println();
+    for (int i = 0; i < sortedArray.length; i++) {
+      System.out.print(sortedArray[i] + " ");
+    }
+
+
 	}
 
 }
